4e29d16036b56143a11f2c7107f7d29ab2a48854
AlexUa99 <2356931k@gmail.com>
2023-01-07 13:57:01 +0200

    asdfa

--- lesson06_object/lesson/main.js
+++ lesson06_object/lesson/main.js
@@ -14,7 +14,7 @@ const arr = [{},{},{}];
 // Обєкт складается з 
 // Ключ + Значення
 
-let year = new Date().getFullYear(); 
+const year = new Date().getFullYear(); 
 
 const user = {
    name: 'Sasha',
@@ -34,8 +34,27 @@ user.coutry = "Ukraine";
 user.name = "Sveta"
 user.phone.huawey = 'm1'
 
+//Деструктуризація
+const {name, age} = user;
+const {ios, android} = user.phone;
+
+console.log(ios);
+console.log(name);
+
 
 //document.write(user.phone.ios);
 console.log(user)
 console.log(user.getFullAge());
 
+
+// Цикл для пперебору обекту
+
+// for(let key in user) {
+//     if (typeof user[key] == 'object') {
+//         for(let key2 in user[key]) {
+//               console.log(`Ключ = ${key2}! Значення = ${user[key][key2]}`);
+//         } 
+//     }else {
+//         console.log(`Ключ = ${key}! Значення = ${user[key]}`);
+//    } 
+// }
